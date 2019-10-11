14338ac8a14bfb7590a97e8a6d464479127b6999
kaollie74 <kaollie74@gmail.com>
2019-10-11 18:27:30 -0500

    initial build of FaceRecognition Component

---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const FaceReognition = () => {
+  return (
+    <div className='center'>
+      <img src={'https://samples.clarifai.com/face-det.jpg' } alt=''/>
+    </div>
+  )
+}
+
+export default FaceReognition;
