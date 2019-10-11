5ea70189cfa6b9cd294be0894fe962a817426455
kaollie74 <kaollie74@gmail.com>
2019-10-11 15:49:43 -0500

    added rank component and imported particles.js background

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const Rank = () => {
+  return (
+    <>
+      <div className='center white f3'>
+        {'Kyle, your current rank is... '}
+      </div>
+      <div className='center white f1'>
+        {'#5'}
+      </div>
+    </>
+  )
+}
+
+export default Rank;
